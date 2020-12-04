ccff2ad52fd4df909c452f86d6cefb70b039274a
xiaohuangyu0120 <354237582@qq.com>
2020-12-04 09:50:48 +0800

    03.test

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 	}
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
 
 }
 //test04
