827c07753e77a82844c65154090fcf5236235b2d
xiaohuangyu0120 <354237582@qq.com>
2021-05-22 13:59:07 +0800

    测试

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -11,6 +11,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 	}
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
 
 }
 //test05
