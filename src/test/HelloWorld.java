d4b183187323e536c7dfa8e6073d482dbf0c31a1
xiaohuangyu0120 <354237582@qq.com>
2021-05-23 09:09:37 +0800

    测试

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -5,16 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 	}
-	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-	}
-	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-	}
-	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-	}
 
 }
-//test05
-//修改测试
+
